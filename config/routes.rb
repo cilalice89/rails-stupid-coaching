8ed9fc95a387cfd1b4499218d08a59f42747cde0
Nacera HAMMID <cilacila4@gmail.com>
2019-08-12 13:30:40 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/question', to: "pages#question", as: :question
+  post 'pages/answer', to: "pages#answer"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
